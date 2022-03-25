3946445dcf2185a1fe4a5792d14f6a89cd7ce3b4
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-25 18:52:09 -0300

    create BookRepository

---
+++ src/main/java/com/levameulivro/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.levameulivro.repository;
+
+import com.levameulivro.models.Book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+    
+}
