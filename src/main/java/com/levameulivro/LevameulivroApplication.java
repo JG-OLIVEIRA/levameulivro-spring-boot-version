9250b9c73dd300cab40ab988415aa9025e1c2d7d
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-25 17:44:30 -0300

    init with spring boot project

---
+++ src/main/java/com/levameulivro/LevameulivroApplication.java
@@ -0,0 +1,13 @@
+package com.levameulivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevameulivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevameulivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levameulivro/LevameulivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levameulivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevameulivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
