d7a7107eb82c71bc3f8c520137df7e89de5ddf4a
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-25 18:53:39 -0300

    create ExchangeRepiository

---
+++ src/main/java/com/levameulivro/repository/ExchangeRepository.java
@@ -0,0 +1,7 @@
+package com.levameulivro.repository;
+
+import com.levameulivro.models.Exchange;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRepository extends JpaRepository<Exchange, Long>{}
