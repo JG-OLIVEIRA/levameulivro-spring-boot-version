24cc851fa9d0a3ddf54f6cae7978a52457d53b4c
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-25 17:57:32 -0300

    fix: adding strategy identity

--- src/main/java/com/levameulivro/models/User.java
+++ src/main/java/com/levameulivro/models/User.java
@@ -1,13 +1,14 @@
 package com.levameulivro.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 import org.springframework.data.annotation.Id;
 
 @Entity
 public class User {
     
-    @Id @GeneratedValue()
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
