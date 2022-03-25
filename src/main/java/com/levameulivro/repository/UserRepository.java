d3ffe6a4065c6b3ac29302b43bb14a9dd03a0ae9
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-25 18:50:24 -0300

    create userRepository

---
+++ src/main/java/com/levameulivro/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.levameulivro.repository;
+
+import com.levameulivro.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{}
