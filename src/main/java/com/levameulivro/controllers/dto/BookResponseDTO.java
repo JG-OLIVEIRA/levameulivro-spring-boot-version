e2bc0b808759d68de392e99ad10115038ad4e449
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-04-04 17:02:57 -0300

    returning field's id

--- src/main/java/com/levameulivro/controllers/dto/BookResponseDTO.java
+++ src/main/java/com/levameulivro/controllers/dto/BookResponseDTO.java
@@ -4,31 +4,46 @@ import com.levameulivro.models.Book;
 
 public class BookResponseDTO {
   
+    private Long id;
     private String owner;
     private String name;
     private String author;
 
     public BookResponseDTO(Book book){
+        this.id = book.getId();
         this.owner = book.getOwner().getName();
         this.name = book.getName();
         this.author = book.getAuthor();
     }
-    
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long bookId){
+        this.id = bookId;
+    }
+
     public String getOwner() {
         return owner;
     }
+
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
--- src/main/java/com/levameulivro/controllers/dto/UserResponseDTO.java
+++ src/main/java/com/levameulivro/controllers/dto/UserResponseDTO.java
@@ -3,12 +3,22 @@ import com.levameulivro.models.User;
 
 public class UserResponseDTO {
     
+    private Long id;
     private String name;
     private String email;
 
     public UserResponseDTO(User user){
+        this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
